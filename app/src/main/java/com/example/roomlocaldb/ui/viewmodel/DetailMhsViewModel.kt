028072f07f9903d29b1f7935f05d5bc6526340b9
hi-lufz1 <latifufauzi@gmail.com>
2024-12-16 12:03:55 +0700

    membuat fungsi yang memindahkan data dri entitiy ke ui

--- app/src/main/java/com/example/roomlocaldb/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/roomlocaldb/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.roomlocaldb.ui.viewmodel
 
+import com.example.roomlocaldb.data.entity.Mahasiswa
+
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isLoading: Boolean = false,
@@ -10,4 +12,18 @@ data class DetailUiState(
         get () = detailUiEvent == MahasiswaEvent()
     val isUiEventNotEmpty: Boolean
         get () = detailUiEvent != MahasiswaEvent()
+}
+
+// Data Class untuk menampung data yang akan ditampilkan di UI
+
+// memindahkan data dri entitiy ke ui
+fun Mahasiswa.toDetailUiEvent() : MahasiswaEvent{
+    return  MahasiswaEvent(
+        nim = nim,
+        nama = nama,
+        jenisKelamin = jenisKelamin,
+        alamat = alamat,
+        kelas = kelas,
+        angkatan = angkatan
+    )
 }
