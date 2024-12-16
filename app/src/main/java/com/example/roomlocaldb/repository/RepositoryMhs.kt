1a72b0a8d7786f0b9c1547d3b2b8d2e34dee1059
hi-lufz1 <latifufauzi@gmail.com>
2024-12-16 10:22:30 +0700

    Update RepositoryMhs

--- app/src/main/java/com/example/roomlocaldb/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/roomlocaldb/repository/RepositoryMhs.kt
@@ -1,7 +1,16 @@
 package com.example.roomlocaldb.repository
 
 import com.example.roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs (mahasiswa: Mahasiswa)
+
+    fun getALlMhs(): Flow<List<Mahasiswa>>
+
+    fun getMhs(nim: String): Flow<Mahasiswa>
+
+    suspend fun deleteMhs (mahasiswa: Mahasiswa)
+
+    suspend fun updateMHs (mahasiswa: Mahasiswa)
 }
